3ca9afdc5709efe36f10b17a0d3c9bc0f497964a
Raunaq Vohra <raunaq.vohra@skyscanner.net>
2020-04-29 22:16:53 +0100

    Refactored to multi-modeule project, closes #1

--- pet-clinic-web/src/test/java/io/github/ronvohra/sfgpetclinic/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/io/github/ronvohra/sfgpetclinic/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package io.github.ronvohra.sfgpetclinic;
+package io.github.ronvohra.sfgpetclinic.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
