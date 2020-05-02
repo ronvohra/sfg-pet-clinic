3d871c25718212544d40cdf09793c6ec9d65bf0f
Raunaq Vohra <raunaq.vohra@skyscanner.net>
2020-05-02 09:46:38 +0100

    Added OwnerController, closes #13

--- pet-clinic-web/src/main/java/io/github/ronvohra/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/io/github/ronvohra/sfgpetclinic/controllers/OwnerController.java
@@ -3,12 +3,12 @@ package io.github.ronvohra.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/owners")
 @Controller
-@RequestMapping("/vets")
-public class VetController {
+public class OwnerController {
 
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
-        return "vets/index";
+    public String listOwners() {
+        return "owners/index";
     }
 }
